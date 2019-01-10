294b9075bf70299a69a2faca113db03e1cb0267d
Roman <romas6ka@i.ua>
2019-01-10 17:35:57 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-main.js
+$(document).ready(function(){
+	$('#navbar2 a').click(function(){
+   		$('#navbar2 a').removeClass("active");
+    	$(this).addClass("active");
+	});
+});
+
+$(window).on('load', function(){
+	$('.btnShop').click(function(){
+		$('.modalHeaderWindow').toggle();
+	})
+})
+
+$(window).on('load', function(){
+	$('.readMore').click(function(){
+		$('#readMore').toggle();
+		$('#noReadMore').toggle();
+	})
+})
