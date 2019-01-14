4bd6b3add054d3a68a0671a4e2c9b532381e66ae
Roman <romas6ka@i.ua>
2019-01-14 10:26:44 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -23,3 +23,88 @@ $(window).on('load', function(){
 		$('#noReadMore').toggle();
 	})
 })
+
+// $('.right>.contentSlider, .left>.contentSlider').carousel({
+//   interval: 1000,
+//   pause: 'hover',
+//   wrap: true
+// })
+
+$(window).on('load', function(){
+	$('.right .contentSlider .prev').click(function(){
+		$('.contentSlider').carousel('prev')
+	})
+})
+
+$(window).on('load', function(){
+	$('.right .contentSlider .next').click(function(){
+		$('.contentSlider').carousel('next')
+	})
+})
+
+$('.carousel1').carousel({
+  interval: 2000,
+  pause: 'hover',
+  wrap: true
+})
+
+$(window).on('load', function(){
+	$('.carousel1 .prev').click(function(){
+		$('.carousel1').carousel('prev')
+	})
+})
+
+$(window).on('load', function(){
+	$('.carousel1 .next').click(function(){
+		$('.carousel1').carousel('next')
+	})
+})
+
+$('.carousel1 .prev, .carousel1 .next').click(function(event) {
+	event.preventDefault();
+});
+
+$('.carouselSlider').carousel({
+  interval: 1000,
+  pause: 'hover',
+  wrap: true
+})
+
+$(window).on('load', function(){
+	$('.carouselSlider .prev').click(function(){
+		$('.carouselSlider').carousel('prev')
+	})
+})
+
+$(window).on('load', function(){
+	$('.carouselSlider .next').click(function(){
+		$('.carouselSlider').carousel('next')
+	})
+})
+
+$('.carouselSlider .prev, .carouselSlider .next').click(function(event) {
+	event.preventDefault();
+});
+
+$('.content>.prev, .content>.next').click(function(event) {
+	event.preventDefault();
+});
+
+// $('.next').click(function(event) {
+// 	event.preventDefault();
+// });
+
+// $('.mySlide').carousel({
+//   interval: 2000,
+//   pause: 'hover',
+//   wrap: true
+// })
+
+
+// $('.mySlide').carousel()
+// $('.mySlide').carousel('next')
+// $('.mySlide').carousel('prev')
+
+// $('.mySlide').on('slide.bs.carousel', function () {
+//   // do something…
+// })
