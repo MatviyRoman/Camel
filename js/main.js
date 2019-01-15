f220143abc16a140761562eb2b50315905dcc87c
Roman <romas6ka@i.ua>
2019-01-15 02:55:17 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -34,77 +34,118 @@ $(window).on('load', function(){
 	$('.right .contentSlider .prev').click(function(){
 		$('.contentSlider').carousel('prev')
 	})
-})
-
-$(window).on('load', function(){
 	$('.right .contentSlider .next').click(function(){
 		$('.contentSlider').carousel('next')
 	})
 })
 
-$('.carousel1').carousel({
-  interval: 2000,
-  pause: 'hover',
-  wrap: true
-})
-
+// sliderCarousel Start
 $(window).on('load', function(){
-	$('.carousel1 .prev').click(function(){
-		$('.carousel1').carousel('prev')
+	$('.sliderCarousel').carousel({
+		interval: 20000,
+		pause: 'hover',
+		wrap: true
 	})
-})
-
-$(window).on('load', function(){
-	$('.carousel1 .next').click(function(){
-		$('.carousel1').carousel('next')
+	$('.sliderCarousel .prev').click(function(){
+		$('.sliderCarousel').carousel('prev')
 	})
+	$('.sliderCarousel .next').click(function(){
+		$('.sliderCarousel').carousel('next')
+	})
+	$('.sliderCarousel .prev, .sliderCarousel .next').click(function(event) {
+		event.preventDefault();
+	});
 })
+// sliderCarousel End
 
-$('.carousel1 .prev, .carousel1 .next').click(function(event) {
-	event.preventDefault();
-});
 
-$('.carouselSlider').carousel({
-  interval: 1000,
-  pause: 'hover',
-  wrap: true
-})
-
+// carouselSlider Start
 $(window).on('load', function(){
+	$('.carouselSlider').carousel({
+	  	interval: 1000,
+	  	pause: 'hover',
+	  	wrap: true
+	})
 	$('.carouselSlider .prev').click(function(){
 		$('.carouselSlider').carousel('prev')
 	})
-})
-
-$(window).on('load', function(){
 	$('.carouselSlider .next').click(function(){
 		$('.carouselSlider').carousel('next')
 	})
+	$('.carouselSlider .prev, .carouselSlider .next').click(function(event) {
+		event.preventDefault();
+	});
 })
-
-$('.carouselSlider .prev, .carouselSlider .next').click(function(event) {
-	event.preventDefault();
-});
-
-$('.content>.prev, .content>.next').click(function(event) {
-	event.preventDefault();
-});
-
-// $('.next').click(function(event) {
-// 	event.preventDefault();
-// });
-
-// $('.mySlide').carousel({
-//   interval: 2000,
-//   pause: 'hover',
-//   wrap: true
-// })
+// carouselSlider End
 
 
-// $('.mySlide').carousel()
-// $('.mySlide').carousel('next')
-// $('.mySlide').carousel('prev')
+// left contentSlider Start
+$(window).on('load', function(){
+	$('.left .contentSlider').carousel({
+	  	interval: 3000,
+	  	pause: 'hover',
+	  	wrap: true
+	})
+	$('.left .contentSlider .prev').click(function(){
+		$('.left .contentSlider').carousel('prev')
+	})
+	$('.left .contentSlider .next').click(function(){
+		$('.left .contentSlider').carousel('next')
+	})
+	$('.left .contentSlider .prev, .left .contentSlider .next').click(function(event) {
+		event.preventDefault();
+	});
+	$('.left .contentSlider img').click(function(){
+   		$('.left .contentSlider img').removeClass("active");
+    	$(this).addClass("active");
+	});
+	var nextSlide = function myStart() { setTimeout( function(){
+        $('.left .contentSlider .prev').removeClass("active");
+    	$('.left .contentSlider .next').addClass("active");
+    	$('.left .contentSlider').carousel('next')
+    },0);}
+    setInterval(nextSlide,3000);
+    var prevSlide = function myStart() { setTimeout( function(){
+        $('.left .contentSlider .next').removeClass("active");
+    	$('.left .contentSlider .prev').addClass("active");
+    	$('.left .contentSlider').carousel('prev')
+    },0);}
+    setInterval(prevSlide,6000);
+})
+// left contentSlider End
 
-// $('.mySlide').on('slide.bs.carousel', function () {
-//   // do something…
-// })
+
+// right contentSlider Start
+$(window).on('load', function(){
+	$('.right .contentSlider').carousel({
+	  	interval: 3500,
+	  	pause: 'hover',
+	  	wrap: true
+	})
+	$('.right .contentSlider .prev1').click(function(){
+		$('.right .contentSlider').carousel('prev')
+	})
+	$('.right .contentSlider .next1').click(function(){
+		$('.right .contentSlider').carousel('next')
+	})
+	$('.right .contentSlider .prev1, .right .contentSlider .next1').click(function(event) {
+		event.preventDefault();
+	});
+	$('.right .contentSlider img').click(function(){
+   		$('.right .contentSlider img').removeClass("active");
+    	$(this).addClass("active");
+	});
+	var nextSlide = function myStart() { setTimeout( function(){
+        $('.right .contentSlider .prev1').removeClass("active");
+    	$('.right .contentSlider .next1').addClass("active");
+    	$('.right .contentSlider').carousel('next')
+    },0);}
+    setInterval(nextSlide,3500);
+    var prevSlide = function myStart() { setTimeout( function(){
+        $('.right .contentSlider .next1').removeClass("active");
+    	$('.right .contentSlider .prev1').addClass("active");
+    	$('.right .contentSlider').carousel('prev')
+    },0);}
+    setInterval(prevSlide,7000);
+})
+// right contentSlider End
